ba588883b18bb4df46a8e24ee43ccb7f4758acbb
halitkalayci <halit.kalayci@ext.etiya.com>
2023-08-01 11:15:58 +0300

    latest

--- server.js
+++ server.js
@@ -3,39 +3,26 @@ const { parse } = require('url')
 const next = require('next')
 
 const dev = process.env.NODE_ENV !== 'production'
-const hostname = 'localhost'
-const port = 3000
-// when using middleware `hostname` and `port` must be provided below
-const app = next({ dev, hostname, port })
+const port = process.env.PORT || 3000;
+const app = next({ dev })
 const handle = app.getRequestHandler()
 
 app.prepare().then(() => {
-    createServer(async (req, res) => {
-        try {
-            // Be sure to pass `true` as the second argument to `url.parse`.
-            // This tells it to parse the query portion of the URL.
-            const parsedUrl = parse(req.url, true)
-            const { pathname, query } = parsedUrl
+    createServer((req, res) => {
+        // Be sure to pass `true` as the second argument to `url.parse`.
+        // This tells it to parse the query portion of the URL.
+        const parsedUrl = parse(req.url, true)
+        const { pathname, query } = parsedUrl
 
-            if (pathname === '/a') {
-                await app.render(req, res, '/a', query)
-            } else if (pathname === '/b') {
-                await app.render(req, res, '/b', query)
-            } else {
-                await handle(req, res, parsedUrl)
-            }
-        } catch (err) {
-            console.error('Error occurred handling', req.url, err)
-            res.statusCode = 500
-            res.end('internal server error')
+        if (pathname === '/a') {
+            app.render(req, res, '/a', query)
+        } else if (pathname === '/b') {
+            app.render(req, res, '/b', query)
+        } else {
+            handle(req, res, parsedUrl)
         }
+    }).listen(port, (err) => {
+        if (err) throw err
+        console.log(`> Ready on http://localhost:${port}`)
     })
-        .once('error', (err) => {
-            console.error(err)
-            process.exit(1)
-        })
-        .listen(port, () => {
-            console.log(`> Ready on http://${hostname}:${port}`)
-        })
-
 })
